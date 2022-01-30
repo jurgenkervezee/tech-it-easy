6eca7a3da6475010cbe482b65eea5eddf46786f7
jurgenkervezee <jurgenbla@gmail.com>
2022-01-30 11:12:45 +0100

    huiswerk deel 1

--- main.js
+++ main.js
@@ -161,3 +161,69 @@ const inventory = [
     sold: 8,
   },
 ];
+
+// #### Opdracht 1 - Array Methoden
+// * **Opdracht 1a:** Gebruik een array-methode om een array te maken met alle tv-type namen. Log de uitkomst in de
+// console.
+const tvNames = inventory.map((tv) => {
+  return tv.name;
+});
+console.log(tvNames);
+
+// * **Opdracht 1b:** Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht
+// zijn. Log de uitkomst in de console.
+const soldTV = inventory.filter((tv) => {
+  return tv.sold === tv.originalStock;
+});
+console.log(soldTV);
+
+// * **Opdracht 1c:** Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight
+// beschikken. Log de uitkomst in de console.
+const ambiLight = inventory.filter((tv) => {
+  return tv.options.ambiLight;
+});
+console.log(ambiLight);
+
+// * **Opdracht 1d:** Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert. Log de uitkomst in de
+// console.
+inventory.sort((a,b) => {
+  return a.price - b.price;
+});
+console.log(inventory);
+
+// #### Opdracht 2 - Elementen in de DOM plaatsen
+// _Tip_: wanneer we meerdere waardes uit een array willen terugbrengen tot één getal of één string, gebruik je hier gewoon
+// een oude vertrouwde for-loop voor!
+// * **Opdracht 2a:** Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+let amountOfSoldTvs = 0;
+for (let i = 0; i < inventory.length ; i++){
+  amountOfSoldTvs += inventory[i].sold;
+}
+console.log("Aantal verkochte tv's is " + amountOfSoldTvs);
+
+// * **Opdracht 2b:** Zorg ervoor dat dit aantal _in het groen_ wordt weergegeven op de pagina.
+const blockOfSoldTV = document.getElementById("amountSoldTVs");
+blockOfSoldTV.innerHTML = `
+                     <p1>Aantal verkochte tv's </p1>
+                     <p1 class="greentv">${amountOfSoldTvs}</p1>`
+
+// * **Opdracht 2c:** Hoeveel tv's heeft Tech It Easy ingekocht? Schrijf een script dat dit berekent. Log de uitkomst in de
+// console.
+let amountOfBoughtTvs = 0;
+for (let i = 0; i < inventory.length ; i++){
+  amountOfBoughtTvs += inventory[i].originalStock;
+}
+console.log("Aantal verkochte tv's is " + amountOfBoughtTvs);
+
+// * **Opdracht 2d:** Zorg ervoor dat dit aantal _in het blauw_ wordt weergegeven op de pagina.
+const blockOfBoughtTV = document.getElementById("amountBoughtTVs");
+blockOfBoughtTV.innerHTML = `
+                     <p1>Aantal gekochte tv's </p1>
+                     <p1 class="bluetv">${amountOfBoughtTvs}</p1>`
+
+// * **Opdracht 2e:** Geef _in het rood_ weer hoeveel tv's er nog verkocht moeten worden.
+// const amountOfStillToSellTvs = amountOfBoughtTvs - amountOfSoldTvs;
+const blockOfStillToSellTV = document.getElementById("amountStillToSellTVs");
+blockOfStillToSellTV.innerHTML = `
+                     <p1>Nog te verkopen tv's </p1>
+                     <p1 class="redtv">${amountOfBoughtTvs - amountOfSoldTvs}</p1>`
